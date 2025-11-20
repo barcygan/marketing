291f2f6883e1d2f954a23e5f9f122f10ba49f6fd
barcygan <bartek.cygan@gmail.com>
2025-11-20 08:26:59 +0100

    Add deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ export default defineConfig({
     // If your repo is https://github.com/username/my-repo, set this to '/my-repo/'
     // For now, we use './' which works for most relative deployments
     base: './',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                pl: 'index-pl.html',
+            },
+        },
+    },
 })
